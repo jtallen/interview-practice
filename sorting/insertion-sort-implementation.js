7e3118f4e4fb5f3b37535c82f669a8314c0da81e
Turner Allen <jturnerallen@gmail.com>
2025-03-03 11:01:37 -0800

    insertion sort implementation

---
+++ sorting/insertion-sort-implementation.js
@@ -0,0 +1,28 @@
+// From Neetcode Problem Implement an Insertion Sort
+// Link: https://neetcode.io/problems/insertionSort
+
+class Solution {
+  /**
+   * @param {Pair[]} pairs
+   * @returns {Pair[][]}
+   */
+  insertionSort(pairs) {
+    let pairsList = pairs.length > 0 ? [pairs] : [];
+    for (let i = 1; i < pairs.length; i++) {
+      let j = i - 1;
+
+      let pairsCopy = [...pairsList[pairsList.length - 1]];
+
+      while (j >= 0 && pairsCopy[j + 1].key < pairsCopy[j].key) {
+        let temp = pairsCopy[j + 1];
+        pairsCopy[j + 1] = pairsCopy[j];
+        pairsCopy[j] = temp;
+        j--;
+      }
+
+      pairsList.push(pairsCopy);
+    }
+
+    return pairsList;
+  }
+}
